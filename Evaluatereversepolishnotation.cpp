d8df40fbe2c975f94571a1e531253811fa8862bb
vaibhavi-roy <97877161+vaibhavi-roy@users.noreply.github.com>
2023-01-02 12:07:48 +0530

    Add files via upload

---
+++ Evaluatereversepolishnotation.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int evalRPN(vector<string> &tokens)
+    {
+        stack<long long> s;
+        for (auto &i : tokens)
+        {
+            if (i == "+" || i == "-" || i == "*" || i == "/")
+            {
+                long long op1 = s.top();
+                s.pop();
+                long long op2 = s.top();
+                s.pop();
+                if (i == "+")
+                    op1 = op2 + op1;
+                if (i == "-")
+                    op1 = op2 - op1;
+                if (i == "*")
+                    op1 = op2 * op1;
+                if (i == "/")
+                    op1 = op2 / op1;
+                s.push(op1);
+            }
+            else
+                s.push(stoll(i)); // string to long long
+        }
+        return s.top();
+    }
+};
