b32dd4ccc3b81ff3cbd71338310e8a5355a206c2
vaibhavi-roy <97877161+vaibhavi-roy@users.noreply.github.com>
2023-01-02 14:03:18 +0530

    Add files via upload

---
+++ Day 2/RotateFunction.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int maxRotateFunction(vector<int> &nums)
+    {
+        int sum = 0, original = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            original += i * nums[i];
+        }
+        int maximum = original;
+        int n = nums.size();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            original += sum - (nums[i] * n);
+            maximum = max(original, maximum);
+        }
+        return maximum;
+    }
+};
