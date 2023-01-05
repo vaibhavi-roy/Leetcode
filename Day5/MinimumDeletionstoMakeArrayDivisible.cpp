7efbc79a4e0c836880d22c9a5fb904e5535fc4e8
vaibhavi-roy <97877161+vaibhavi-roy@users.noreply.github.com>
2023-01-05 22:26:35 +0530

    Add files via upload

---
+++ Day5/MinimumDeletionstoMakeArrayDivisible.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, vector<int> &numsDivide)
+    {
+        int g = numsDivide[0];
+        for (auto it : numsDivide)
+            g = gcd(g, it);
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() && nums[i] <= g; i++)
+            if (g % nums[i] == 0)
+                return i;
+        return -1;
+    }
+};
