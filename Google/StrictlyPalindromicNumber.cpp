6e5a8e37161b720604ff9bd6774215a5cd7f8916
Vaibhavi Roy <97877161+vaibhavi-roy@users.noreply.github.com>
2023-01-24 15:46:09 +0530

    Add files via upload

---
+++ Google/StrictlyPalindromicNumber.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    bool palindrome(string &s)
+    {
+        int p = 0;
+        int q = s.size() - 1;
+        while (p < q)
+        {
+            if (s[p] == s[q])
+            {
+                p++;
+                q--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isStrictlyPalindromic(int n)
+    {
+        for (int i = 2; i <= n - 2; i++)
+        {
+            int p = n;
+            string s;
+            while (p > 0)
+            {
+                s.push_back(p % i);
+                p /= i;
+            }
+            if (!palindrome(s))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
